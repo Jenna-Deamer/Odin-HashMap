cfdd734caeb6d1d9a3831f9e216eb776beb3ccf6
Jenna Deamer <jennadeamer@gmail.com>
2025-08-03 20:56:14 -0400

    Initailcommit with hashMap factory with hash function

---
+++ script.js
@@ -0,0 +1,16 @@
+function hashMap() {
+    const loadFactor;
+    const capacity;
+
+    function hash(key) {
+        let hashCode = 0;
+
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+            hashCode = primeNumber * hashCode + key.charCodeAt(i);
+        }
+
+        return hashCode;
+    }
+    return;
+}
