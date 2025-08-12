179d846ef9d94b0422d8957a87033d22356e2351
Jenna Deamer <jennadeamer@gmail.com>
2025-08-12 13:39:47 -0400

    Add value update when a key is found in a bucket, Add append new value if there are values in the bucket with no matches

--- hashMap.js
+++ hashMap.js
@@ -7,7 +7,6 @@ export function hashMap() {
 
     function hash(key) {
         let hashCode = 0;
-
         const primeNumber = 31;
         for (let i = 0; i < key.length; i++) {
             hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % capacity;
@@ -22,20 +21,35 @@ export function hashMap() {
         }
 
         const pair = { key: key, value: value }
-        console.log(pair)
-
+      
+        // Bucket has items: Search for key update if found. Append if not
         if (buckets[index]) {
-            console.log('Found')
+            console.log('bucket contains items')
             let list = buckets[index];
-          
-            // Find linked list that contains the key
-            // Update value with new value
+            let currentNode = list.currentHead();
+            let length = list.currentSize()
+
+            for (let i = 0; i <= length; i++) {
+
+                if (currentNode.val.key === key) {
+                    console.log('Match found - updating value')
+                    currentNode.val.value = value;
+                    break;
+                } else if (i === length - 1) {
+                    console.log("No match - appending")
+                    list.append(pair);
+                    buckets.splice(index, 0, list)
+                }
+                else {
+                    currentNode = currentNode.nextNode
+                }
+            }
         }
+
+        // Bucket is empty: create linked list & append pair 
         if (buckets[index] === undefined) {
-            console.log(buckets[index])
-            // Create linked list
+            console.log('Bucket empty - creating linked list')
             const list = LinkedList();
-            // Append pair
             list.append(pair);
             buckets.splice(index, 0, list)
         }
--- main.js
+++ main.js
@@ -4,4 +4,5 @@ import { hashMap } from "./hashMap.js";
 const test = hashMap();
 
 test.set('apple', 'red')
+test.set('j', 'red')
 test.set('apple', 'New red')
